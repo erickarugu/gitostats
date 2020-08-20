4a129aa12803b5a0818621f9712c825f38179e45
erickarugu32 <erickarugu32@gmail.com>
2020-08-20 16:18:08 +0300

    Added Charts

--- app/js/script.js
+++ app/js/script.js
@@ -1,41 +1,7 @@
 import renderCharts from "./charts.js";
+import displayUserInfo from "./profile.js";
 
 const APIURL = "https://api.github.com/users/";
-
-const userObject = {
-  login: "erickarugu32",
-  id: 26389470,
-  node_id: "MDQ6VXNlcjI2Mzg5NDcw",
-  avatar_url: "https://avatars3.githubusercontent.com/u/26389470?v=4",
-  gravatar_id: "",
-  url: "https://api.github.com/users/erickarugu32",
-  html_url: "https://github.com/erickarugu32",
-  followers_url: "https://api.github.com/users/erickarugu32/followers",
-  following_url: "https://api.github.com/users/erickarugu32/following{/other_user}",
-  gists_url: "https://api.github.com/users/erickarugu32/gists{/gist_id}",
-  starred_url: "https://api.github.com/users/erickarugu32/starred{/owner}{/repo}",
-  subscriptions_url: "https://api.github.com/users/erickarugu32/subscriptions",
-  organizations_url: "https://api.github.com/users/erickarugu32/orgs",
-  repos_url: "https://api.github.com/users/erickarugu32/repos",
-  events_url: "https://api.github.com/users/erickarugu32/events{/privacy}",
-  received_events_url: "https://api.github.com/users/erickarugu32/received_events",
-  type: "User",
-  site_admin: false,
-  name: "Eric Karugu",
-  company: "Freelance",
-  blog: "https://erickarugu32.github.io",
-  location: "Nairobi, Kenya",
-  email: null,
-  hireable: true,
-  bio: "I am a passionate web developer based in Nairobi, Kenya.\r\n\r\nSkills: JavaScript, WordPress, Linux, Git, Angular, MongoDB, Node.js, UI/UX",
-  twitter_username: "karugu_eric",
-  public_repos: 34,
-  public_gists: 0,
-  followers: 6,
-  following: 6,
-  created_at: "2017-03-13T17:55:23Z",
-  updated_at: "2020-08-18T07:52:04Z",
-};
 
 const hideLoader = (e) => {
   setTimeout(() => {
@@ -45,39 +11,8 @@ const hideLoader = (e) => {
   }, 2000);
 };
 
-const fetchUserInfo = async (username) => {
-  await fetch(`${url}/${username}`)
-    .then((data) => data.json())
-    .then((data) => displayUserGithubInfo(data));
-};
-
-const displayUserGithubInfo = (userObject) => {
-  const userGithubAvatar = document.querySelector("#user_avatar");
-  const userFullname = document.querySelector("#user_fullname");
-  const userUsername = document.querySelector("#user_username");
-  const userWork = document.querySelector("#user_work");
-  const userLocation = document.querySelector("#user_location");
-  const userJoinDate = document.querySelector("#user_join-date");
-  const userRepos = document.querySelector("#user_repos");
-  const userFollowers = document.querySelector("#user_followers");
-  const userFollowing = document.querySelector("#user_following");
-
-  var joinedDate = new Date(userObject.created_at);
-
-  userGithubAvatar.src = userObject.avatar_url;
-  userFullname.innerText = userObject.name;
-  userUsername.innerText = `@${userObject.login}`;
-  userWork.innerText = userObject.company;
-  userLocation.innerText = userObject.location;
-  userJoinDate.innerText = joinedDate.toDateString();
-  userRepos.innerText = userObject.public_repos;
-  userFollowers.innerText = userObject.followers;
-  userFollowing.innerText = userObject.following;
-};
-
-document.addEventListener("DOMContentLoaded", (e) => {
+window.onload = (e) => {
+  displayUserInfo(APIURL, "erickarugu32");
+  renderCharts(APIURL, "erickarugu32");
   hideLoader();
-  displayUserGithubInfo(userObject);
-});
-
-renderCharts(userObject);
+};
