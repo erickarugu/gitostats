5b6d221156a4ae35eeacc24080f18600008721dd
erickarugu32 <erickarugu32@gmail.com>
2020-08-22 02:54:47 +0300

    Connected to the real github API + added sort feature

--- app/js/profile.js
+++ app/js/profile.js
@@ -1,48 +1,4 @@
-export default function (url, username, displayRequestsLimit) {
-  const userObject = {
-    login: "erickarugu32",
-    id: 26389470,
-    node_id: "MDQ6VXNlcjI2Mzg5NDcw",
-    avatar_url: "https://avatars3.githubusercontent.com/u/26389470?v=4",
-    gravatar_id: "",
-    url: "https://api.github.com/users/erickarugu32",
-    html_url: "https://github.com/erickarugu32",
-    followers_url: "https://api.github.com/users/erickarugu32/followers",
-    following_url: "https://api.github.com/users/erickarugu32/following{/other_user}",
-    gists_url: "https://api.github.com/users/erickarugu32/gists{/gist_id}",
-    starred_url: "https://api.github.com/users/erickarugu32/starred{/owner}{/repo}",
-    subscriptions_url: "https://api.github.com/users/erickarugu32/subscriptions",
-    organizations_url: "https://api.github.com/users/erickarugu32/orgs",
-    repos_url: "https://api.github.com/users/erickarugu32/repos",
-    events_url: "https://api.github.com/users/erickarugu32/events{/privacy}",
-    received_events_url: "https://api.github.com/users/erickarugu32/received_events",
-    type: "User",
-    site_admin: false,
-    name: "Eric Karugu",
-    company: "Freelance",
-    blog: "https://erickarugu32.github.io",
-    location: "Nairobi, Kenya",
-    email: null,
-    hireable: true,
-    bio: "I am a passionate web developer based in Nairobi, Kenya.\r\n\r\nSkills: JavaScript, WordPress, Linux, Git, Angular, MongoDB, Node.js, UI/UX",
-    twitter_username: "karugu_eric",
-    public_repos: 34,
-    public_gists: 0,
-    followers: 6,
-    following: 6,
-    created_at: "2017-03-13T17:55:23Z",
-    updated_at: "2020-08-18T07:52:04Z",
-  };
-
-  const fetchUserInfo = async (url, username) => {
-    await fetch(`${url}/${username}`)
-      .then((data) => {
-        displayRequestsLimit(data);
-        return data.json();
-      })
-      .then((data) => displayUserGithubInfo(data));
-  };
-
+export default function (userObject) {
   const displayUserGithubInfo = (userObject) => {
     const userGithubAvatar = document.querySelector("#user_avatar");
     const userFullname = document.querySelector("#user_fullname");
--- app/js/script.js
+++ app/js/script.js
@@ -2,15 +2,18 @@ import displayUserInfo from "./profile.js";
 import renderCharts from "./charts.js";
 import displayRepositories from "./repositories.js";
 
-var username = "daniel";
+const username = "erickarugu32";
 const APIURL = "https://api.github.com/users";
+var userObject = {};
+var reposObject = {};
+var sortBy = "name";
+const reposLimit = 12;
 
 const displayRequestsLimit = (data) => {
   const requestsMaximum = document.querySelector("#req_maximum");
   const requestsRemaining = document.querySelector("#req_remaining");
 
   for (var pair of data.headers.entries()) {
-    console.log(pair[0] + ": " + pair[1]);
     if (pair[0] === "x-ratelimit-limit") {
       requestsMaximum.innerText = pair[1];
     }
@@ -20,7 +23,32 @@ const displayRequestsLimit = (data) => {
   }
 };
 
-const hideLoader = (e) => {
+const fetchUserRepositories = async (url, username) => {
+  await fetch(`${url}/${username}/repos`)
+    .then((data) => {
+      displayRequestsLimit(data);
+      return data.json();
+    })
+    .then((repositoryObject) => {
+      reposObject = repositoryObject;
+      renderCharts(repositoryObject);
+      displayRepositories(repositoryObject, sortBy, reposLimit);
+    });
+};
+
+const fetchUserInfo = async (url, username) => {
+  await fetch(`${url}/${username}`)
+    .then((data) => {
+      displayRequestsLimit(data);
+      return data.json();
+    })
+    .then((responseObject) => {
+      userObject = responseObject;
+      displayUserInfo(responseObject);
+    });
+};
+
+const hideLoader = () => {
   setTimeout(() => {
     const loader = document.querySelector(".box");
     document.body.style.overflowY = "scroll";
@@ -30,8 +58,21 @@ const hideLoader = (e) => {
 
 window.onload = (e) => {
   document.title = "GitoStats | Eric Karugu";
-  displayUserInfo(APIURL, username, displayRequestsLimit);
-  renderCharts(APIURL, username, displayRequestsLimit);
-  displayRepositories(APIURL, username, displayRequestsLimit);
+  const sortByButton = document.querySelector("#sort_by");
+  const usernameInput = document.querySelector("#github_name");
+  const searchButton = document.querySelector("#search__btn");
+
+  searchButton.addEventListener("click", (e) => {
+    console.log(usernameInput.value);
+    if (usernameInput.value) {
+      fetchUserInfo(APIURL, usernameInput.value);
+    }
+  });
+  sortByButton.addEventListener("change", (e) => {
+    displayRepositories(reposObject, e.target.value, reposLimit);
+  });
+
   hideLoader();
+  fetchUserInfo(APIURL, username);
+  fetchUserRepositories(APIURL, username);
 };
